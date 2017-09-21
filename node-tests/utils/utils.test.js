bbb24b5847a8f7aeb619aca555fbbc51748a248c
gabriel <gabrieldornianu@yahoo.com>
2017-09-21 14:53:53 +0300

    Simulating async functions testing

--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -8,20 +8,40 @@ it('Should add two numbers', () => {
   expect(res).toBe(11).toBeA('number');
 });
 
+it('Should async add two numbers', (done) => {
+  utils.asyncAdd(3, 7, (sum) => {
+    expect(sum).toBe(10).toBeA('number');
+    done();
+  });
+});
+
 it('Should square a number', () => {
   var res = utils.square(4);
 
   expect(res).toBe(16).toBeA('number');
 });
 
-it('Should expect some values', () => {
-  // expect({name: 'Gabi'}).toEqual({name: 'Gabi'});
-  // expect([2, 3, 4]).toExclude(3);
-  expect({
-    name: 'Gabi',
-    age: 22,
-    location: 'Ro'
-  }).toInclude({
-    age: 22
+it('Should async square a number', (done) => {
+  utils.asyncSquare(9, (sqr) => {
+    expect(sqr).toEqual(81).toBeA('number');
+    done(); 
   });
 });
+
+it('Should verify first and last name', () => {
+  var user = {};
+  var userWithName = utils.setName(user, 'Dornianu Gabriel');
+  expect(user.firstName).toEqual('Dornianu');
+});
+
+// it('Should expect some values', () => {
+//   expect({name: 'Gabi'}).toEqual({name: 'Gabi'});
+//   expect([2, 3, 4]).toExclude(3);
+//   expect({
+//     name: 'Gabi',
+//     age: 22,
+//     location: 'Ro'
+//   }).toInclude({
+//     age: 22
+//   });
+// });
